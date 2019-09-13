a605d52a37bbd5830e23325154fd1415ffca059a
fet <fetmar@gmail.com>
2019-09-13 16:09:45 -0400

    working test

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
 import React from 'react'
-import { mapping } from '@eva-design/eva'
-import { ApplicationProvider } from 'react-native-ui-kitten'
-import { DynamicStatusBar } from './ui/components/common'
-import { Router } from './ui/core/navigation/Routes'
-import { ThemeContext, ThemeStore, themes } from '@src/core/themes'
-import SettingsStore from './data/stores/SettingsStore'
+import { View, Text } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
-export default class App extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      theme: 'Eva Dark'
-    }
-  }
-
-  componentDidMount = async () => {
-    await SettingsStore.initialize()
-  }
-
-  onTransitionTrackError = error => {
-    console.warn('Analytics error: ', error.message)
-  }
-
-  onSwitchTheme = theme => {
-    ThemeStore.setTheme(theme).then(() => {
-      this.setState({ theme })
-    })
-  }
-
+class HomeScreen extends React.Component {
   render () {
-    const contextValue = {
-      currentTheme: this.state.theme,
-      toggleTheme: this.onSwitchTheme
-    }
-
     return (
-      <ThemeContext.Provider value={contextValue}>
-        <ApplicationProvider mapping={mapping} theme={themes[this.state.theme]}>
-          <DynamicStatusBar currentTheme={this.state.theme} />
-          <Router />
-        </ApplicationProvider>
-      </ThemeContext.Provider>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
     )
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+})
+
+export default createAppContainer(AppNavigator)
