0a77cc1e29d4e6d77f4a1c7ebdbb68243dc6a816
Kristofer Pelchat <kris.pelchat@oneiro.io>
2019-10-11 12:42:36 -0400

    final work to get CryptoStore plugged in

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import AppNavigation from '@src/core/navigation/AppNavigation'
 import SettingsStore from '@src/data/stores/SettingsStore'
 import { useScreens } from 'react-native-screens'
 import AsyncStorage from '@react-native-community/async-storage'
-import { GeneralStore } from 'ndaujs'
+import { GeneralStore, CryptoStore } from 'ndaujs'
+import { generateSecureRandom } from 'react-native-securerandom'
 
 // Some libraries haven't removed componentWillMount yet
 YellowBox.ignoreWarnings(['Warning: componentWillMount'])
@@ -13,6 +14,7 @@ useScreens()
 
 global.Keyaddr = NativeModules.KeyAddressManager
 GeneralStore.default.setStore(AsyncStorage)
+CryptoStore.default.setStore(generateSecureRandom)
 
 export default class App extends React.Component {
   constructor (props) {
