7355235a34382d2678b2dbef414cc9a1e856d77f
Ed McNierney <ed@mcnierney.com>
2020-02-11 10:48:18 -0500

    Create/update copyright messages

--- src/ui/core/navigation/AppNavigation.js
+++ src/ui/core/navigation/AppNavigation.js
@@ -1,3 +1,13 @@
+/* ----- ---- --- -- -
+ * Copyright 2020 The Axiom Foundation. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.apache.org/licenses/LICENSE-2.0.txt
+ * - -- --- ---- -----
+ */
+
 import React from 'react'
 import { createSwitchNavigator, createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
